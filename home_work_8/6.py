db0ffe4efa16129b26dd3cb484e8f5aa00694ba0
Ekaterina <ekaterina.piryakova@mail.ru>
2020-11-02 19:15:55 +0300

    tertools.combinations

---
+++ home_work_8/6.py
@@ -0,0 +1,12 @@
+import itertools
+
+def get_combinations(s, n):
+    array = []
+    for i in range(1,n+1):
+        for item in itertools.combinations(s, i):
+            array.append(''.join(item))
+
+    print(array)
+
+
+get_combinations("cat", 2)  
